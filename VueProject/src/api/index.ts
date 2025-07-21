6af10ab91fd5615a1f3db60102ef08d1a5b3b75f
Your Name <your@email.com>
2025-07-21 22:26:50 +0300

    fix: исправлен baseURL для API, теперь используется https://dkor.pro/api

--- VueProject/src/api/index.ts
+++ VueProject/src/api/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import type { AxiosRequestConfig } from 'axios';
 
 const instance = axios.create({
-  baseURL: '/', // Замените на базовый URL вашего API, если необходимо
+  baseURL: 'https://dkor.pro/api', 
   timeout: 10000, // Пример таймаута
   headers: {
     'Content-Type': 'application/json',
